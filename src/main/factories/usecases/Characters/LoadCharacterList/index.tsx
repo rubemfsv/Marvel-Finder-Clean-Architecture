8c2ed0443884c1f7da1ed43f15e97ffcd3e975f5
Rubem <rfsv@ic.ufal.br>
2021-04-01 16:22:50 -0300

    chore: adjust limit

--- src/main/factories/usecases/Characters/LoadCharacterList/index.tsx
+++ src/main/factories/usecases/Characters/LoadCharacterList/index.tsx
@@ -5,7 +5,7 @@ import { makeAxiosHttpClient, makeApiUrl } from '../../../http';
 export const makeLoadCharacterList = (hero: string): ILoadCharacterList => {
   const remoteLoadCharacterList = new RemoteLoadCharacterList(
     makeApiUrl(
-      `/characters?ts=1&apikey=cc3a419a1f142ca1a1230ff360c178ee&hash=e6be44dd2255fdf5720f087595275f0c${
+      `/characters?limit=90&ts=1&apikey=cc3a419a1f142ca1a1230ff360c178ee&hash=e6be44dd2255fdf5720f087595275f0c${
         hero && `&name=${hero}`
       }`
     ),
