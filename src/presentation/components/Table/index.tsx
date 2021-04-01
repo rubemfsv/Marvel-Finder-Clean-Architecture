96aeddd2e024f9b5cae274290b0f1119bf1e7db5
Rubem <rfsv@ic.ufal.br>
2021-04-01 16:35:34 -0300

    feature: create Table component

---
+++ src/presentation/components/Table/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Character } from '@/domain/models';
+
+import Styles from './styles.scss';
+import HeroCard from './HeroCard';
+
+type TableProps = {
+  charactersArray: Character.Model[];
+};
+
+const Table: React.FC<TableProps> = ({ charactersArray }: TableProps) => {
+  return (
+    <table className={Styles.tableContent}>
+      <thead>
+        <tr>
+          <th>Personagem</th>
+          <th>Séries</th>
+          <th>Eventos</th>
+        </tr>
+      </thead>
+      <tbody>
+        {charactersArray.map((character: Character.Model, index: number) => {
+          return <HeroCard character={character} index={index} />;
+        })}
+      </tbody>
+    </table>
+  );
+};
+
+export default Table;
