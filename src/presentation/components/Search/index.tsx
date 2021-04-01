cf40e1c954c49a2fe3dade744e1a351dc2275161
Rubem <rfsv@ic.ufal.br>
2021-04-01 00:22:17 -0300

    chore: make search functional

--- src/presentation/components/Search/index.tsx
+++ src/presentation/components/Search/index.tsx
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Styles from './styles.scss';
 
-const Search: React.FC = () => {
+type SearchProps = {
+  getInputResult: (text: string) => void;
+};
+
+const Search: React.FC<SearchProps> = ({ getInputResult }: SearchProps) => {
+  const [inputValue, setInputValue] = useState('');
+  const [blurred, setBlurred] = useState(true);
+
+  useEffect(() => {
+    getInputResult(inputValue);
+  }, [inputValue]);
+
+  const handleInputChange = (
+    event: React.FocusEvent<HTMLInputElement>
+  ): void => {
+    setInputValue(event.target.value);
+  };
+
   return (
     <div className={Styles.searchContainer}>
       <label className={Styles.searchLabel}>Nome do Personagem</label>
-      <input className={Styles.searchInput} />
+      <input
+        className={Styles.searchInput}
+        onChange={handleInputChange}
+        onFocus={() => setBlurred(false)}
+        onBlur={() => setBlurred(true)}
+      />
     </div>
   );
 };
--- src/presentation/pages/Dashboard/index.tsx
+++ src/presentation/pages/Dashboard/index.tsx
@@ -1,6 +1,6 @@
 import { ILoadCharacterList } from '@/domain/usecases';
 import { Header, Search } from '@/presentation/components';
-import React, { useEffect } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 
 import Styles from './styles.scss';
 
@@ -11,16 +11,22 @@ type DashboardProps = {
 const Dashboard: React.FC<DashboardProps> = ({
   loadCharacterList,
 }: DashboardProps) => {
+  const [search, setSearch] = useState('');
   useEffect(() => {
-    loadCharacterList('')
+    loadCharacterList(search)
       .load()
       .then((response) => console.log(response))
       .catch((error) => console.log(error));
+  }, [search]);
+
+  const getSearchResult = useCallback((inputValue: string) => {
+    setSearch(inputValue);
   }, []);
+
   return (
     <div className={Styles.dashboard}>
       <Header />
-      <Search />
+      <Search getInputResult={getSearchResult} />
     </div>
   );
 };
