77191515fec01fc5452c84627ed37316d1ed7681
Rubem <rfsv@ic.ufal.br>
2021-03-31 23:28:24 -0300

    chore: adjust LoadCharacters filter rule

--- src/main/factories/usecases/Characters/LoadCharacterList/index.tsx
+++ src/main/factories/usecases/Characters/LoadCharacterList/index.tsx
@@ -2,12 +2,12 @@ import { RemoteLoadCharacterList } from '@/data/usecases';
 import { ILoadCharacterList } from '@/domain/usecases';
 import { makeAxiosHttpClient, makeApiUrl } from '../../../http';
 
-export const makeLoadUserFollowersList = (
-  filter: string
-): ILoadCharacterList => {
+export const makeLoadCharacterList = (hero: string): ILoadCharacterList => {
   const remoteLoadCharacterList = new RemoteLoadCharacterList(
     makeApiUrl(
-      `/characters?ts=1&apikey=cc3a419a1f142ca1a1230ff360c178ee&hash=e6be44dd2255fdf5720f087595275f0c&name=${filter}`
+      `/characters?ts=1&apikey=cc3a419a1f142ca1a1230ff360c178ee&hash=e6be44dd2255fdf5720f087595275f0c${
+        hero && `&name=${hero}`
+      }`
     ),
     makeAxiosHttpClient()
   );
