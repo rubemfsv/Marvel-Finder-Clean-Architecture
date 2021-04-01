8d80ef011b97161c806bca31c57a70b8934ade92
Rubem <rfsv@ic.ufal.br>
2021-04-01 10:22:28 -0300

    refactor: uses Character.Model namespace instead of CharacterModel

--- src/domain/usecases/Characters/LoadCharacterList.ts
+++ src/domain/usecases/Characters/LoadCharacterList.ts
@@ -1,9 +1,9 @@
-import { CharacterModel, HttpResponseModel } from 'domain/models/';
+import { Character, HttpResponseModel } from 'domain/models/';
 
 export interface ILoadCharacterList {
   load: () => Promise<HttpResponseModel<LoadCharacterList.Model[]>>;
 }
 
 export namespace LoadCharacterList {
-  export type Model = CharacterModel
+  export type Model = Character.Model
 }
