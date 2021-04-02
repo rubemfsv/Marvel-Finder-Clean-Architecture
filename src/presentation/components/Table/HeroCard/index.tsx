606d72650fb548c0c03c3f677055aa4ff2e0f9b2
Rubem <rfsv@ic.ufal.br>
2021-04-01 23:26:22 -0300

    chore: adjust character info container

--- src/presentation/components/Table/HeroCard/index.tsx
+++ src/presentation/components/Table/HeroCard/index.tsx
@@ -12,9 +12,19 @@ const HeroCard: React.FC<HeroCardProps> = ({
   character,
   index,
 }: HeroCardProps) => {
+  console.log(character);
   return (
     <tr key={index} className={Styles.tableBodyItem}>
-      <td>{character.name}</td>
+      <td>
+        <div className={Styles.characterInfoContainer}>
+          <img
+            className={Styles.characterAvatar}
+            src={`${character.thumbnail.path}.jpg`}
+            alt={character.name}
+          />
+          <span className={Styles.characterName}>{character.name}</span>
+        </div>
+      </td>
       <td>{character.series.available}</td>
       <td>{character.events.available}</td>
     </tr>
