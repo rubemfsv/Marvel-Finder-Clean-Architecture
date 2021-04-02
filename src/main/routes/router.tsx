0286ccdec0bcffd1e47994909c49cf715325f2a9
Rubem <rfsv@ic.ufal.br>
2021-04-02 18:32:18 -0300

    chore: injects HeroDetails factory to routes

--- src/main/routes/router.tsx
+++ src/main/routes/router.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import { makeDashboard } from '@/main/factories/pages/';
+import { makeDashboard, makeHeroDetails } from '@/main/factories/pages/';
 
 const Router: React.FC = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={makeDashboard} />
+        <Route exact path="/details/:id" component={makeDashboard} />
       </Switch>
     </BrowserRouter>
   );
