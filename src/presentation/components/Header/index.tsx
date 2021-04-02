74e7bd03cceeb6c86f7f1be502a00e8e92b1cd41
Rubem <rfsv@ic.ufal.br>
2021-04-02 14:55:26 -0300

    chore: adjust border header

--- src/presentation/components/Header/index.tsx
+++ src/presentation/components/Header/index.tsx
@@ -6,9 +6,11 @@ const Header: React.FC = () => {
   return (
     <div className={Styles.header}>
       <div className={Styles.systemTitleContainer}>
-        <span className={Styles.systemTitleBorder}>BUS</span>
-        <span className={Styles.systemTitleBold}>CA MARVEL</span>
-        <span className={Styles.systemTitleRegular}>TESTE FRONT-END</span>
+        <div>
+          <span className={Styles.systemTitleBold}>BUSCA MARVEL</span>
+          <span className={Styles.systemTitleRegular}>TESTE FRONT-END</span>
+        </div>
+        <div className={Styles.line} />
       </div>
       <span className={Styles.systemTitleRegular}>@RUBEMFSV</span>
     </div>
