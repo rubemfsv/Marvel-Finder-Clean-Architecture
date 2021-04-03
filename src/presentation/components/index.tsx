7da324a266e57053100c6ec183ead502ddd16a5c
Rubem <rfsv@ic.ufal.br>
2021-04-03 11:46:40 -0300

    chore: put button at the index of the components

--- src/presentation/components/index.tsx
+++ src/presentation/components/index.tsx
@@ -1,3 +1,4 @@
+export { default as Button } from './Button';
 export { default as Header } from './Header';
 export { default as Pagination } from './Pagination';
 export { default as Search } from './Search';
